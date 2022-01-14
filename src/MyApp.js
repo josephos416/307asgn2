d2efe075a46e09ae219c19a43d56b963b0775311
Joseph Perez <jpere219@calpoly.edu>
2022-01-14 13:01:40 -0800

    works fine b4 step 6

--- src/MyApp.js
+++ src/MyApp.js
@@ -34,6 +34,23 @@ function removeOneCharacter (index) {
     });
     setCharacters(updated);
   }
+  function updateList(person) {
+   makePostCall(person).then( result => {
+   if (result && result.status === 200)
+      setCharacters([...characters, person] );
+   });
+}
+
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:5000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+ }
 
 
 return (
